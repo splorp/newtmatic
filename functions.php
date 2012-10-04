95aeadaf8e8add1bfaf62eee471e505f77e8efca
Grant Hutchinson <grant@splorp.com>
2012-10-04 15:38:51 -0600

    Docmentation and comment updates

--- functions.php
+++ functions.php
@@ -1,15 +1,11 @@
 <?php
+/**
+ * @package WordPress
+ * @subpackage Thematic
+ */
+?>
 
-//
-//  Custom Child Theme Functions
-//
-
-// I've included a "commented out" sample function below that'll add a home link to your menu
-// More ideas can be found on "A Guide To Customizing The Thematic Theme Framework" 
-// http://themeshaper.com/thematic-for-wordpress/guide-customizing-thematic-theme-framework/
-
-// Adds a home link to your menu
-// http://codex.wordpress.org/Template_Tags/wp_page_menu
+<?php
 
 function childtheme_menu_args($args) {
     $args = array(
